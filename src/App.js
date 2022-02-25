4dd34b24784c663289594393a952cf82df28ba55
Gustavo <gusfeh07@gmail.com>
2022-02-25 11:59:05 -0300

    feat: create sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
     <div className="app">
       <h1>Discord clone</h1>
+      <Sidebar />
     </div>
   );
 }
